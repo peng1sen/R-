60debc1d9e2f64fad752f75f93ee4b5f923dface
彭奕森 <44118329+peng1sen@users.noreply.github.com>
2019-05-02 16:16:17 +0800

    add library

--- Hss_heavey and low damping.R
+++ Hss_heavey and low damping.R
@@ -1,13 +1,15 @@
 ##
+library(readxl)
 library(ggplot2)
 library(grid)
 #
 HSS_straight_low <- read_excel("Downloads/test00000.xlsx")
 #
 set.seed(5)
-HSS_straight_low.subset <- HSS_straight_low[sample(nrow(HSS_straight_low), 10000),] 
-chart_1 = qplot(Time, f, data = HSS_straight_low.subset,alpha = I(1/100))+
-  ggtitle("low damping")+
+HSS_straight_low.subset <- HSS_straight_low[sample(nrow(HSS_straight_low),1000),] 
+chart_1 = qplot(HSS_straight_low.subset$`Time, s`, HSS_straight_low.subset$`LT LOWER TRAP., uV`, data = HSS_straight_low.subset,alpha = I(1/10))+
+  ggtitle("lulu")+
   xlab('Time')+
   ylab('uV')
+chart_1
 ##
